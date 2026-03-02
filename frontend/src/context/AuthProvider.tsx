04313558323c119fdc6f81eddb1f3c95e3d69039
Pavan Vamsi <168455353+pavanvamsi1@users.noreply.github.com>
2026-03-02 13:58:42 +0530

    Add files via upload

---
+++ frontend/src/context/AuthProvider.tsx
@@ -0,0 +1,29 @@
+import { useState, type ReactNode } from "react";
+import { AuthContext, type User } from "./AuthContext";
+
+interface Props {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<User | null>(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const login = (userData: User) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
