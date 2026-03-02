625e2d5e603cb2ee1d32fd0f541ac0b32e34ee23
Pavan Vamsi <168455353+pavanvamsi1@users.noreply.github.com>
2026-03-02 13:59:52 +0530

    Update ProtectedRoute.tsx

--- frontend/src/components/auth/ProtectedRoute.tsx
+++ frontend/src/components/auth/ProtectedRoute.tsx
@@ -1,20 +1,19 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../context/useAuth";
+import type { ReactElement } from "react";
 
 interface Props {
-  children: JSX.Element;
-  allowedRoles?: string[];
+  children: ReactElement;
+  allowedRoles?: ("ADMIN" | "EMPLOYEE")[];
 }
 
 const ProtectedRoute = ({ children, allowedRoles }: Props) => {
   const { user } = useAuth();
 
-  // Not logged in
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // Logged in but role not allowed
   if (allowedRoles && !allowedRoles.includes(user.role)) {
     return <Navigate to="/" replace />;
   }
