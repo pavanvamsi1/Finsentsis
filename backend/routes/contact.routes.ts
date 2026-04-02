79324ec74e8d3eabd3659461baa336544e52b60d
Pavan Vamsi <168455353+pavanvamsi1@users.noreply.github.com>
2026-04-02 20:24:44 +0530

    Revert "Added Email Notification Module (OTP & Contact Confirmation APIs)"

--- backend/routes/contact.routes.ts
+++
@@ -1,8 +0,0 @@
-import express = require("express");
-import { contactHandler } from "../controllers/contact.controller";
-
-const router = express.Router();
-
-router.post("/contact", contactHandler);
-
-export = router;
